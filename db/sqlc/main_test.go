d18f01a9beecef828a125a7a8972e63310c8ac07
paperhsiaooo <paper.hsiaooo@gmail.com>
2024-05-04 12:07:19 +0800

    test: 新增 unit-test case for insert data to account

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5"
+
+	// Import the postgres driver，但不會實際使用它，避免 format 時被移除，所以加上 _
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	ctx := context.Background()
+	conn, err := pgx.Connect(ctx, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueries = New(conn) // Pass conn as a pointer to DBTX
+
+	os.Exit(m.Run())
+}
